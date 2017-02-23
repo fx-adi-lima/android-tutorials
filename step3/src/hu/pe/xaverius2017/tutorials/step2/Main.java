fdf8204a52a378b9edf6cfc2c892f8e6e6b1505c
Adi Lima <xaverius2017@gmail.com>
2017-02-23 20:45:21 +0700

    bikin screenshots dulu, setelah bikin split screen dengan edittext

--- step3/src/hu/pe/xaverius2017/tutorials/step2/Main.java
+++ step3/src/hu/pe/xaverius2017/tutorials/step2/Main.java
@@ -6,6 +6,10 @@ import android.content.Context;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.ScrollView;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.view.Gravity;
 
 
 public class Main extends Activity {
@@ -16,16 +20,34 @@ public class Main extends Activity {
 		
 		LinearLayout ll = new LinearLayout(this);
 		ll.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-		ll.setOrientation(LinearLayout.VERTICAL);
+		ll.setOrientation(LinearLayout.HORIZONTAL);
 		setContentView(ll);
 
+		ScrollView sv = new ScrollView(this);
+		LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+		sv.setLayoutParams(params);
+		ll.addView(sv);
+
+		LinearLayout contents = new LinearLayout(this);
+		contents.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0.5f));
+		contents.setOrientation(LinearLayout.VERTICAL);
+		contents.setBackgroundColor(0xffffffff);
+		sv.addView(contents);
+
 		TextView tv = null;
+		float fsize = 75;
 		for (int i = 0; i < 25; i++) {
 			tv = new TextView(this);
-			tv.setTextSize(30);
+			tv.setTextSize(fsize);
 			tv.setText("TextView nomor " + i + " baru dibuat");
-			ll.addView(tv);
+			contents.addView(tv);
+			fsize -= 1.1f;
 		}
+
+		EditText et = new EditText(this);
+		et.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		et.setGravity(Gravity.TOP);
+		ll.addView(et);
 	}
 }
 
